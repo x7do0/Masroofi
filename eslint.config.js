83b76151c032a7c3c073bb01d0ce0267f922cf96
x7do0 <x7do02@gmail.com>
2026-08-07 14:16:52 +0300

    fix: simplify eslint dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,7 @@
-import js from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   { ignores: ['dist'] },
-  js.configs.recommended,
   ...tseslint.configs.recommended,
   {
     files: ['**/*.{ts,tsx}'],
