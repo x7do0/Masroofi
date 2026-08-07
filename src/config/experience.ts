4c36663c5910c163aa2262386415f08980541d3a
x7do0 <x7do02@gmail.com>
2026-08-07 15:21:37 +0300

    feat: add experience configuration

---
+++ src/config/experience.ts
@@ -0,0 +1,7 @@
+export const LOW_BALANCE_THRESHOLD_IQD = 100_000;
+
+export const appExperience = {
+  lowBalanceThreshold: LOW_BALANCE_THRESHOLD_IQD,
+  toastDurationMs: 2600,
+  animationDurationMs: 420,
+} as const;
